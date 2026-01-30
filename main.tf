b7d380bfc0997e5799e82f1c0cd12b0c89105681
DavidKroll <david@2network-it.com>
2026-01-30 20:20:51 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-west-1"
 }
 
 module "Vms_app1" {
   source = "git::https://github.com/sivanick88/tf-aws-modules-jan26-siva.git//ec2-instance?ref=main"
-  region-1 = "ap-south-1"
+  region-1 = "us-west-1"
   instance_type = "t3.micro"
   subnets = ["subnet-084785ce11bb38827", "subnet-0e100c7e306c1812d"]
   num_of_vm = 1
